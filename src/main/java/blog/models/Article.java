fef2641764fcdb40b4f8bd4bdab4ab2cc615dd51
Antoine Fissot <antoine@semifir.com>
2021-11-17 11:14:05 +0100

    Update: Model Article

--- src/main/java/blog/models/Article.java
+++ src/main/java/blog/models/Article.java
@@ -1,5 +1,32 @@
 package blog.models;
 
 public class Article {
+	
+	private String title;
+	private String content;
+	private User user;
+	public Article(String title, String content, User user) {
+		this.setContent(content);
+		this.setTitle(title);
+		this.setUser(user);
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 }
